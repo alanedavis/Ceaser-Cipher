e896033517a22856bd7e8ddf10211b9ddd208b60
alanedavis <alan.davis.el@gmail.com>
2019-05-22 02:59:29 -0400

    Decrypting is confusing

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import random
-import string
-
-def genData():
-    data = ""
-
-    for num in range(0,dataLength):
-        data += random.choice(string.ascii_letters)
+import re
     
-    return data
-    
-def encode(key):
-    pass
+def encrypt(text,key):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+
+        if text[i] == " ":
+            result += " "
+            continue
+        elif char.isupper():
+            result += chr((ord(char) + key - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) + key - 97) % 26 + 97)
+        
+    return result
 
 if __name__ == "__main__":
-
-    dataLength = random.randint(2,100)
+    text = input("Enter any sentence you'd like: ")
+    re.sub('[^A-Za-z0-9]+', '', text)
     key = random.randint(1,25)
-
-    data = genData()
-
-    print(data)
+    print("Text         : " + text)
+    print("Key          : " + str(key))
+    print("Cipher       : " + encrypt(text,key))
